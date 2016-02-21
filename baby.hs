d0aa1b23130b7fb58b9464e34bd8a4e178c11c5e
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2016-02-21 23:28:31 +0100

    going through to the end of chapter 1

--- baby.hs
+++ baby.hs
@@ -6,6 +6,31 @@ doubleSmallNumber x = if x<100
     else x
 tripleSmallNumber x = if x<50 then tripleMe x else doubleMe x
 doubleMe' x = succ(x+x)
+
 andrzej = "Andrzej R"
 myNumbers = [11,22,33,44,55,66,77,88,98]
 myString = "Warad-Shamash"
+o = myNumbers
+
+tailsHead x = head(tail x)
+initsLast x = last(init x)
+reversedMiddle x = reverse(init(tail x))
+
+evenList = [2,4,6,8]
+oddList = [1,3,5,7,9]
+
+getMiddle x = x !! ((length x) `div` 2)
+prMinMax x = product([minimum x,maximum x])
+range x = maximum x - minimum x
+
+-- now better lists using ranges!
+betterEvenList = [2,4..100]
+betterOddList = [1,3..99]
+
+infiniteList = [1,2..]
+
+repeatIsReplicate x y = take y (repeat x) == replicate y x
+squaresIfMod42is13 x = [z*z | z<-[1..x], z`mod`42==13]
+
+nestedList = [[1,2,3,4], [5,6,7,8,9]]
+removeOddFromNested xxs = [[x | x <- xs, even x] | xs <- xxs ]
--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,1 +1,6 @@
 fizzbuzz x = if x `mod` 3 == 0 then (if x `mod` 5 == 0 then "FizzBuzz" else "Fizz") else if x `mod` 5 == 0 then "Buzz" else show x
+
+-- trying to make it more functional
+fizz3 x = if x `mod` 3 == 0 then "Fizz" else show x
+fizz5 x = if x `mod` 5 == 0 then "Buzz" else show x
+fizzbuzzBetter x = if (fizz3 x == fizz5 x)
